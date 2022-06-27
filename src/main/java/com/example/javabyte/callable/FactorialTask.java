d2e85f57afa46d079540be982bbd349aa6e2a7ad
Umaralikhon <umaralikhon@gmail.com>
2022-06-27 17:15:32 +0500

    Callable

---
+++ src/main/java/com/example/javabyte/callable/FactorialTask.java
@@ -0,0 +1,24 @@
+package com.example.javabyte.callable;
+
+import java.util.concurrent.Callable;
+
+public class FactorialTask implements Callable<Integer> {
+
+    private int num;
+
+    public FactorialTask(int num){
+        this.num = num;
+    }
+
+    @Override
+    public Integer call() {
+
+        int fact = 1;
+
+        for (int i = num; i > 1; i--) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+}
